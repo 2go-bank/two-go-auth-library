cf4488ac2daab5f9505ca39ec093ce20a4a9b72e
Joao G Zanon Jr <Joao Zanon>
2024-11-01 20:29:36 -0300

    Ajuste no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,24 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
-import {resolve} from 'path'
-import {componentTagger} from "lovable-tagger";
 
-
-export default defineConfig(({mode}) => ({
-    server: {
-        host: "::",
-        port: 8080,
-    },
+export default defineConfig({
     plugins: [
         react(),
-        mode === 'development' && componentTagger(),
         dts({
             insertTypesEntry: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src')
-        }
-    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: '2goBaseAuth',
+            name: '2go-base-auth',
             formats: ['es', 'umd'],
-            fileName: (format) => `2go-base-auth.${format}.js`,
+            fileName: (format) => `2go-base-auth.${format}.js`
         },
-        outDir: 'dist',
-        emptyOutDir: true,
         rollupOptions: {
-            external: [],
-            input: {
-                main: resolve(__dirname, 'index.html'),
-            },
+            external: ['react', 'react-dom'],
             output: {
                 globals: {
                     react: 'React',
@@ -44,4 +27,17 @@ export default defineConfig(({mode}) => ({
             },
         },
     },
-}));
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    define: {
+        'process.env': {
+            VITE_AUTH_API_URL: JSON.stringify('https://api.2gopag.com/access/auth'),
+            VITE_API_BASE_URL: JSON.stringify('https://api.2gopag.com'),
+            VITE_ENVIRONMENT: JSON.stringify('production'),
+            VITE_LOGO_URL: JSON.stringify('https://2gobank.com.br/wp-content/uploads/2023/05/logo-2go-bank.png')
+        }
+    }
+})
